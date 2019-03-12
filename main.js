2d9bf25d2e4edd0403d0e445ad712521d137196e
Klaus Gerlicher <klaus@klausgerlicher.de>
2019-03-12 11:20:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,121 @@
+/**
+ *
+ * alphaadvantage adapter
+ *
+ *
+ *  file io-package.json comments:
+ *
+ *  {
+ *      "common": {
+ *          "name":         "alphaadv",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
+ *          "version":      "0.0.0",                    // use "Semantic Versioning"! see http://semver.org/
+ *          "title":        "Node.js alphaadv Adapter",  // Adapter title shown in User Interfaces
+ *          "authors":  [                               // Array of authord
+ *              "name <mail@alphaadv.com>"
+ *          ]
+ *          "desc":         "alphaadvantage adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
+ *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
+ *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
+ *          "materialize":  true,                       // support of admin3
+ *          "schedule":     "0 0 * * *"                 // cron-style schedule. Only needed if mode=schedule
+ *          "loglevel":     "info"                      // Adapters Log Level
+ *      },
+ *      "native": {                                     // the native object is available via adapter.config in your adapters code - use it for configuration
+ *          "test1": true,
+ *          "test2": 42,
+ *          "mySelect": "auto"
+ *      }
+ *  }
+ *
+ */
+
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+'use strict';
+
+// you have to require the utils module and call adapter function
+const utils = require(__dirname + '/lib/utils'); // Get common adapter utils
+
+// you have to call the adapter function and pass a options object
+// name has to be set and has to be equal to adapters folder name and main file name excluding extension
+// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.alphaadv.0
+const adapter = new utils.Adapter('alphaadv');
+
+const https = require('https');
+const request = require('request');
+const util = require('util')
+const prettyMs = require('pretty-ms');
+
+var g_lights = []
+
+function format(fmt, ...args) {
+    if (!fmt.match(/^(?:(?:(?:[^{}]|(?:\{\{)|(?:\}\}))+)|(?:\{[0-9]+\}))+$/)) {
+        throw new Error('invalid format string.');
+    }
+    return fmt.replace(/((?:[^{}]|(?:\{\{)|(?:\}\}))+)|(?:\{([0-9]+)\})/g, (m, str, index) => {
+        if (str) {
+            return str.replace(/(?:{{)|(?:}})/g, m => m[0]);
+        } else {
+            if (index >= args.length) {
+                throw new Error('argument index is out of range in format');
+            }
+            return args[index];
+        }
+    });
+}
+
+function toPaddedHexString(num, len) {
+    let str = num.toString(16);
+    return "0".repeat(len - str.length) + str;
+}
+
+// is called when adapter shuts down - callback has to be called under any circumstances!
+adapter.on('unload', function (callback) {
+    try {
+        adapter.log.info('cleaned everything up...');
+        callback();
+    } catch (e) {
+        callback();
+    }
+});
+
+// is called when databases are connected and adapter received configuration.
+// start here!
+adapter.on('ready', function () {
+    main();
+});
+
+adapter.on('stateChange', function (id, state) {
+});
+
+function create_indicator(name, description, value) {
+    adapter.getObject(name, function(err, obj) { 
+        if (!obj) {
+            adapter.setObject(name, {
+                type: 'state',
+                common: {
+                    name: description,
+                    role: 'state',
+                    type: "boolean",
+                    "read":  true,
+                    "write": false
+                    },
+                native: {}
+            });
+            adapter.setState(name, value, true);
+        }
+    });
+    adapter.setState(name, value, true);
+}
+
+function main() {
+
+    // The adapters config (in the instance object everything under the attribute "native") is accessible via
+    // adapter.config:
+    adapter.log.info('config APIKEY: ' + adapter.config.apikey);
+
+    // in this all states changes inside the adapters namespace are subscribed
+    adapter.subscribeStates('*');
+
+    init_device();
+}
