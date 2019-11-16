f59a62ed8f80a3fceaec4907cbbada8670366047
Klaus Gerlicher <klaus@klausgerlicher.de>
2019-11-16 15:17:23 +0100

    make some progress quickly (not working fully)

--- main.js
+++ main.js
@@ -42,11 +42,8 @@ const utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 const adapter = new utils.Adapter('alphaadv');
 
 const https = require('https');
-const request = require('request');
 const util = require('util')
 const prettyMs = require('pretty-ms');
-
-var g_lights = []
 
 function format(fmt, ...args) {
     if (!fmt.match(/^(?:(?:(?:[^{}]|(?:\{\{)|(?:\}\}))+)|(?:\{[0-9]+\}))+$/)) {
@@ -62,11 +59,6 @@ function format(fmt, ...args) {
             return args[index];
         }
     });
-}
-
-function toPaddedHexString(num, len) {
-    let str = num.toString(16);
-    return "0".repeat(len - str.length) + str;
 }
 
 // is called when adapter shuts down - callback has to be called under any circumstances!
@@ -92,7 +84,7 @@ function create_indicator(name, description, value) {
     adapter.getObject(name, function(err, obj) { 
         if (!obj) {
             adapter.setObject(name, {
-                type: 'state',
+                type: value == null ? '' : 'state',
                 common: {
                     name: description,
                     role: 'state',
@@ -102,10 +94,94 @@ function create_indicator(name, description, value) {
                     },
                 native: {}
             });
-            adapter.setState(name, value, true);
+            if (value != null) {
+                adapter.setState(name, value, true);
+            }
         }
     });
     adapter.setState(name, value, true);
+}
+
+function get_api_key() {
+    return adapter.config.apikey.trim();
+}
+
+function get_function_url(symbol, _function) {
+    return 'https://www.alphavantage.co/query?function=' + _function + '&datatype=json&symbol=' + symbol + '&apikey=' + get_api_key();    
+}
+
+// generalised query, calls decoder(callback) from cb_data
+function query(url, cb_data) {
+    https.get(url, function(res){
+        var body = '';
+    
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+    
+        res.on('end', function(){
+            var fbResponse = JSON.parse(body);
+            console.log("Got a response: ", fbResponse);
+            cb_data.decoder(fbResponse, cb_data.cb);
+    });
+    }).on('error', function(e){
+        console.log("Got an error: ", e);
+    });    
+}
+
+function get_global_quote_decoder(fbResponse, cb) {
+    var data = fbResponse["Global Quote"]            
+
+    var symbol = data['01. symbol'];
+    var open = data['02. open'];
+    var high = data['03. high'];
+    var low = data['04. low'];
+    var price = data['05. price'];
+    var vol  = data['06. volume'];
+    var date = data['07. latest trading day'];
+    var prev = data['08. previous close'];
+    var change = data['09. change'];
+    var change_percent = data['10. change percent'];
+    cb({
+        'symbol': symbol,
+        'open': open, 
+        'high': high, 
+        'low': low, 
+        'price': price, 
+        'vol': vol, 
+        'date': date, 
+        'prev': prev, 
+        'change': change, 
+        'percent': change_percent
+    });
+}
+
+function create_global_quote_states(data) {
+    adapter.log.debug(data);
+    create_indicator('symbols', 'Stock symbols', null);
+    create_indicator('symbols.day', 'GLOBAL_QUOTES', null);
+    for (var key in data) {
+        create_indicator('symbols.day.' + key + '.date', null, data[key]);
+    }
+}
+
+function get_global_quote(symbol) {
+    adapter.log.info('get_global_quote(): ' + symbol);
+
+    var url = get_function_url(symbol, 'GLOBAL_QUOTE');
+    var cb_data = {
+        'cb': create_global_quote_states,
+        'decoder': get_global_quote_decoder
+    };
+
+    query(url, cb_data);
+}
+
+function update_stock_data() {
+    adapter.config.symbols.forEach( function(item, index) {
+        get_global_quote(item.trim());
+    });
+    setTimeout(update_stock_data, 15*1000);
 }
 
 function main() {
@@ -116,4 +192,44 @@ function main() {
 
     // in this all states changes inside the adapters namespace are subscribed
     adapter.subscribeStates('*');
+
+    // fill up adapter objects
+    update_stock_data();
 }
+
+
+
+
+/// 
+
+//function get_time_series_daily(symbol) {
+    //     adapter.log.info('get_aa(): ' + symbol);
+    //     var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&datatype=json&symbol=' + symbol + '&interval=1min&apikey=' + adapter.config.apikey.trim();
+    //     https.get(url, function(res){
+    //         var body = '';
+        
+    //         res.on('data', function(chunk){
+    //             body += chunk;
+    //         });
+        
+    //         res.on('end', function(){
+    //             var fbResponse = JSON.parse(body);
+    //             console.log("Got a response: ", fbResponse);
+    //             var meta_data = fbResponse["Meta Data"];
+    //             var data = fbResponse["Time Series (Daily)"]            
+    //             var last_refreshed = meta_data['3. Last Refreshed'];
+    //             var i = data[last_refreshed];
+    //             var value_open = i['1. open'];
+    //             var value_close = i['4. close'];
+    //             var volume = i['5. volume'];
+    //             create_indicator('symbols', 'Stock symbols', null);
+    //             create_indicator('symbols.' + symbol + '.date', 'Date', last_refreshed);
+    //             create_indicator('symbols.' + symbol + '.open', 'Value at open', value_open);
+    //             create_indicator('symbols.' + symbol + '.close', 'Value at close', value_close);
+    //             create_indicator('symbols.' + symbol + '.volume', 'Days volume', volume);
+    //     });
+    //     }).on('error', function(e){
+    //         console.log("Got an error: ", e);
+    //     });    
+    // }
+    
